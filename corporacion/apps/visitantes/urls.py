07dec73757e1ae7b933300353f6c8e2954def0f1
mundosport <mundosportec@gmail.com>
2018-02-05 22:27:22 -0500

    visitantes

--- corporacion/apps/visitantes/urls.py
+++ corporacion/apps/visitantes/urls.py
@@ -1,7 +1,6 @@
 
 from django.conf.urls import url
-from django.contrib import admin
-from apps.eventos.views import AspNuevo, EvnLista, MsgNuevo
+from apps.visitantes.views import AspNuevo, EvnLista, MsgNuevo
 
 
 urlpatterns = [
--- corporacion/apps/visitantes/views.py
+++ corporacion/apps/visitantes/views.py
@@ -1,9 +1,7 @@
 from django.core.urlresolvers import reverse_lazy
 from django.views.generic import CreateView, ListView
-from apps.eventos.forms import MensajeForm, EventoForm, AspiranteForm
-
-from apps.eventos.models import Zipcodigo, Aspirante, Evento, Mensaje
-
+from apps.eventos.forms import MensajeForm, AspiranteForm
+from apps.eventos.models import Aspirante, Evento, Mensaje
 
 # ======== M  E  N  S  A  J  E  =========== #
 
@@ -12,8 +10,8 @@ class MsgNuevo(CreateView):
     """Crear Mensaje"""
     model = Mensaje
     form_class = MensajeForm
-    template_name = 'eventos/Msg_New.html'
-    success_url = reverse_lazy('eventos:msg_panel')
+    template_name = 'visitantes/Msg_New.html'
+    success_url = reverse_lazy('visitantes:msg_panel')
 
 # ========  E  V  E  N  T  O  S  =========== #
 
@@ -21,7 +19,7 @@ class MsgNuevo(CreateView):
 class EvnLista(ListView):
     """Listado de Evento"""
     model = Evento
-    template_name = 'eventos/Evn_Panel.html'
+    template_name = 'visitantes/Evn_Panel.html'
     ordering = ['pk']
     paginate_by = 15
 
@@ -32,5 +30,5 @@ class AspNuevo(CreateView):
     """Crear Aspirante"""
     model = Aspirante
     form_class = AspiranteForm
-    template_name = 'eventos/Asp_New.html'
-    success_url = reverse_lazy('eventos:asp_panel')
+    template_name = 'visitantes/Asp_New.html'
+    success_url = reverse_lazy('visitantes:asp_panel')
